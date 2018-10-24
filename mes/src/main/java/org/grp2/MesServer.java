e875b9e150984cb4b093a0a4f33ed7a73562149a
Dennis Petersen <Dennis-drengen@hotmail.dk>
2018-10-24 07:16:13 +0200

    common folder

--- mes/src/main/java/org/grp2/MesServer.java
+++ mes/src/main/java/org/grp2/MesServer.java
@@ -1,13 +1,7 @@
 package org.grp2;
 
-/**
- * Hello world!
- *
- */
-public class MesServer
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class MesServer {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
--- scada/src/main/java/org/grp2/ScadaServer.java
+++ scada/src/main/java/org/grp2/ScadaServer.java
@@ -1,13 +1,7 @@
 package org.grp2;
 
-/**
- * Hello world!
- *
- */
-public class ScadaServer
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class ScadaServer {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
