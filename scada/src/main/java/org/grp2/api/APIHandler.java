a7357474e449dee426f8a65b9990b7381640e52f
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2018-11-07 11:34:18 +0100

    checks if beers are missing

--- scada/src/main/java/org/grp2/api/APIHandler.java
+++ scada/src/main/java/org/grp2/api/APIHandler.java
@@ -28,6 +28,7 @@ public class APIHandler {
         message.send(context);
     }
 
+
     public void manageProduction(Context context) {
         String choice = context.pathParam("choice");
         Message message = new Message(200, "");
@@ -93,5 +94,4 @@ public class APIHandler {
 
         context.json(map);
     }
-
 }
