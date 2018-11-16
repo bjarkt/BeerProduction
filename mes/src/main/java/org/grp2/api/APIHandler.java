f38f594db2efde1f6f0a06434bfe99403d1ca104
bjarkt <bjarke.tobiesen@gmail.com>
2018-11-16 12:06:01 +0100

    fix mes api

--- mes/src/main/java/org/grp2/api/APIHandler.java
+++ mes/src/main/java/org/grp2/api/APIHandler.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
