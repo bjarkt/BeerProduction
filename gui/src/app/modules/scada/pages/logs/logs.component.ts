74d62fae441a1e87cf7554fa9e731c21c6277a86
Dennis Petersen <Dennis-drengen@hotmail.dk>
2018-11-16 13:46:29 +0100

    Paginator added to logs

--- gui/src/app/modules/scada/pages/logs/logs.component.ts
+++ gui/src/app/modules/scada/pages/logs/logs.component.ts
@@ -1,22 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { PageEvent, MatTableDataSource, MatPaginator } from '@angular/material';
+import { PageEvent, MatTableDataSource, MatPaginator, MatTable } from '@angular/material';
 
 export interface LogElement {
   temperature: number;
   humidity: number;
   vibration: number;
-}
-
-const DATA: LogElement[] = [];
-
-for (let index = 0; index < 20; index++) {
-  const data: LogElement = {
-    temperature: Math.random() * 5 * (index + 1),
-    humidity: Math.random() * 5 * (index + 1),
-    vibration: Math.random() * 2 * (index + 1)
-  };
-
-  DATA.push(data);
 }
 
 @Component({
@@ -26,16 +14,47 @@ for (let index = 0; index < 20; index++) {
 })
 
 export class LogsComponent implements OnInit {
+  totalRefresh: number;
+  elementData: LogElement[] = [];
   displayedColumns: String[] = ['temperature', 'humidity', 'vibration'];
-  dataSource = new MatTableDataSource<LogElement>(DATA);
+  dataSource = new MatTableDataSource<LogElement>(this.elementData);
+  @ViewChild(MatTable) table: MatTable<LogElement>;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-
+  
   constructor() {
-
+    this.totalRefresh = 0;
   }
 
   ngOnInit() {
+    for (let index = 0; index < 20; index++) {
+      const data: LogElement = {
+        temperature: (index + 1) * (this.totalRefresh + 1),
+        humidity: (index + 1) * (this.totalRefresh + 1),
+        vibration: (index + 1) * (this.totalRefresh + 1)
+      };
+
+      this.elementData.push(data);
+    }
+
     this.dataSource.paginator = this.paginator;
+  }
+
+  refreshLogs() {
+    this.totalRefresh++;
+    this.elementData = [];
+
+    for (let index = 0; index < 20; index++) {
+      const data: LogElement = {
+        temperature: (index + 1) * (this.totalRefresh + 1),
+        humidity: (index + 1) * (this.totalRefresh + 1),
+        vibration: (index + 1) * (this.totalRefresh + 1)
+      };
+
+      this.elementData.push(data);
+    }
+
+    this.dataSource.data = this.elementData;
+    this.table.renderRows();
   }
 
 }
