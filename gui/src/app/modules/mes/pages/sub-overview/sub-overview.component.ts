ea5422ec87c3ec5a07ce0fff8cac22316212f795
bjarkt <bjarke.tobiesen@gmail.com>
2018-11-19 14:42:28 +0100

    mes overview

--- gui/src/app/modules/mes/pages/sub-overview/sub-overview.component.ts
+++ gui/src/app/modules/mes/pages/sub-overview/sub-overview.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Batch } from 'src/app/shared/models/Batch';
 import { Order } from 'src/app/shared/models/order';
+import { MesDataService } from 'src/app/shared/services/mesData.service';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-sub-overview',
@@ -10,12 +12,36 @@ import { Order } from 'src/app/shared/models/order';
 export class SubOverviewComponent implements OnInit {
 
   batches: Batch[] = [];
-  orders: Order[] = [];
+  batchesDataSource: MatTableDataSource<Batch> = new MatTableDataSource();
+  batchesColumnsToDisplay = ['batchId', 'orderNumber', 'beerName', 
+                      'accepted', 'defect', 'started', 'finished'];
 
-  constructor() { }
+  orders: Order[] = [];
+  ordersDataSource: MatTableDataSource<Order> = new MatTableDataSource();
+  ordersColumnsToDisplay = ['orderNumber', 'date', 'status'];
+
+  constructor(private data: MesDataService) { }
 
   ngOnInit() {
-    // fetch batch, order
+    this.loadData();
+  }
+
+  public async loadData() {
+    this.batches = await this.data.getBatches().toPromise() as Batch[]
+    this.batches.sort((a, b) => {
+      if (a.batchId > b.batchId) return -1;
+      else if (a.batchId < b.batchId) return 1;
+      return 0;
+    })
+    this.batchesDataSource.data = this.batches;
+
+    this.orders = await this.data.getMesOrders().toPromise() as Order[]
+    this.orders.sort((a, b) => {
+      if (a.orderNumber > b.orderNumber) return -1;
+      else if (a.orderNumber < b.orderNumber) return 1;
+      return 0;
+    })
+    this.ordersDataSource.data = this.orders
   }
 
 }
