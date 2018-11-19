d04b1555044ba380d60e1c0da4548028ff817e47
Dennis Petersen <Dennis-drengen@hotmail.dk>
2018-11-19 09:35:33 +0100

    Added state log beside measure log

--- gui/src/app/modules/scada/pages/logs/logs.component.ts
+++ gui/src/app/modules/scada/pages/logs/logs.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { PageEvent, MatTableDataSource, MatPaginator, MatTable } from '@angular/material';
 
-export interface LogElement {
+export interface MeasureElement {
+  datetime: string;
   temperature: number;
   humidity: number;
   vibration: number;
+}
+
+export interface StateElement {
+  phase: string;
+  timeElapsed: number;
 }
 
 @Component({
@@ -15,46 +21,71 @@ export interface LogElement {
 
 export class LogsComponent implements OnInit {
   totalRefresh: number;
-  elementData: LogElement[] = [];
-  displayedColumns: String[] = ['temperature', 'humidity', 'vibration'];
-  dataSource = new MatTableDataSource<LogElement>(this.elementData);
-  @ViewChild(MatTable) table: MatTable<LogElement>;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  elementMeasureData: MeasureElement[] = [];
+  elementStateData: StateElement[] = [];
+  displayedMeasureColumns: String[] = ['datetime', 'temperature', 'humidity', 'vibration'];
+  displayedStateColumns: String[] = ['phase', 'timeElapsed'];
+  dataMeasureSource = new MatTableDataSource<MeasureElement>(this.elementMeasureData);
+  dataStateSource = new MatTableDataSource<StateElement>(this.elementStateData);
+
+  @ViewChild('measuretable') MeasureTable: MatTable<MeasureElement>;
+  @ViewChild('statetable') stateTable: MatTable<StateElement>;
+  @ViewChild('measurepaginator') measurePaginator: MatPaginator;
+  @ViewChild('statepaginator') statePaginator: MatPaginator;
   
   constructor() {
     this.totalRefresh = 0;
   }
 
   ngOnInit() {
+    this.addTemporaryMeasures();
+    this.addTemporaryStates();
+
+    this.dataMeasureSource.paginator = this.measurePaginator;
+    this.dataStateSource.paginator = this.statePaginator;
+
+    this.dataMeasureSource.data = this.elementMeasureData;
+    this.dataStateSource.data = this.elementStateData;
+  }
+
+  addTemporaryMeasures() {
+    this.elementMeasureData = [];
+
     for (let index = 0; index < 20; index++) {
-      const data: LogElement = {
+      const data: MeasureElement = {
+        datetime: '08-11-2018 12:00:00',
         temperature: (index + 1) * (this.totalRefresh + 1),
         humidity: (index + 1) * (this.totalRefresh + 1),
         vibration: (index + 1) * (this.totalRefresh + 1)
       };
 
-      this.elementData.push(data);
+      this.elementMeasureData.push(data);
     }
+  }
 
-    this.dataSource.paginator = this.paginator;
+  addTemporaryStates() {
+    this.elementStateData = [];
+
+    for (let index = 0; index < 7; index++) {
+      const data: StateElement = {
+        phase: 'Phase ' + index,
+        timeElapsed: this.totalRefresh * index
+      };
+
+      this.elementStateData.push(data);
+    }
   }
 
   refreshLogs() {
     this.totalRefresh++;
-    this.elementData = [];
 
-    for (let index = 0; index < 20; index++) {
-      const data: LogElement = {
-        temperature: (index + 1) * (this.totalRefresh + 1),
-        humidity: (index + 1) * (this.totalRefresh + 1),
-        vibration: (index + 1) * (this.totalRefresh + 1)
-      };
+    this.addTemporaryMeasures();
+    this.addTemporaryStates();
 
-      this.elementData.push(data);
-    }
-
-    this.dataSource.data = this.elementData;
-    this.table.renderRows();
+    this.dataMeasureSource.data = this.elementMeasureData;
+    this.dataStateSource.data = this.elementStateData;
+    
+    this.MeasureTable.renderRows();
+    this.stateTable.renderRows();
   }
-
 }
