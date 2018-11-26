e22b8fdf2067af5d0c3161d686e38af807d9a82e
bjarkt <bjarke.tobiesen@gmail.com>
2018-11-26 11:40:43 +0100

    add PWA to angular for caching

--- gui/src/main.ts
+++ gui/src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
+//if (environment.production) {
   enableProdMode();
-}
+//}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('ngsw-worker.js')
+    }
+  })
   .catch(err => console.error(err));
+
